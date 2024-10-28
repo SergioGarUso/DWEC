539716c1edd16565a8b239b92771739b4f872152
SergioGarUso <sergaruso@alu.edu.gva.es>
2024-10-28 10:27:31 +0100

    1.1

--- P5/Ej5/Ej5.js
+++ P5/Ej5/Ej5.js
@@ -2,16 +2,17 @@ class Contador {
     constructor() {
         this.valor = 0;
     }
-
+    //incrementa el numero
     incrementar() {
         this.valor++;
     }
-
+    //devuelve el valor 
     mostrar() {
         return this.valor;
     }
 }
 
+//creamos el objeto y creamos la funcion que usara el boton
 const contador = new Contador();
 function incrementa() {
     contador.incrementar();
