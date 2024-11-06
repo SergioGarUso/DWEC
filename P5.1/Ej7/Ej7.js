5bf31f9f6b001e6f522e5dd7a63cc15c020f63bc
SergioGarUso <sergaruso@alu.edu.gva.es>
2024-11-06 08:38:23 +0100

    1.1

--- P5.1/Ej7/Ej7.js
+++ P5.1/Ej7/Ej7.js
@@ -1,27 +1,42 @@
 let punto = {
-    _x: 0,
+    x: 0,
     _y: 0,
     _z: 0,
-
+    _cuadrante: 0,
     set y(x){
         this._y = x * 2;
     },
-
+    get y(){
+        return this._y;
+    },
     set z(valor){
         this._z = Math.sqrt(this.x*valor);
     },
-
-    _cuadrante: 0,
-
+    get z(){
+        return this._z;
+    },
     get cuadrante(){
-        if(this._x > 0 && this._y > 0) return 1;
-        if(this._x < 0 && this._y > 0) return 2;
-        if(this._x < 0 && this._y < 0) return 3;
-        if(this._x > 0 && this._y < 0) return 4;
+        if(this.x > 0 && this._y > 0) return 1;
+        if(this.x < 0 && this._y > 0) return 2;
+        if(this.x < 0 && this._y < 0) return 3;
+        if(this.x > 0 && this._y < 0) return 4;
     },
     distancia(){
-        return Math.sqrt(Math.pow(this._x,2)+Math.pow(this._y,2));
+        return Math.sqrt(Math.pow(this.x,2)+Math.pow(this._y,2));
     },
-    
+    mostrar_cordenadas(){
+        console.log(`Cordenadas : ${this.x} , ${this._y} , ${this._z}`); 
+    },
+    mostrar_cuadrante(){
+        console.log(`Cuadrante es : ${this._cuadrante}`);
+    }
     
 }
+
+punto.x = 7;
+punto._y = punto.x;
+punto._z = 11;
+
+console.log("La distancia es:" + punto.distancia());
+punto.mostrar_cordenadas();
+punto.mostrar_cuadrante();
